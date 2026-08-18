4a4d1382bafeec261e319a7744e2afcb0983520c
alan33ifce <alan.martins63@aluno.ifce.edu.br>
2026-08-18 17:45:18 -0300

    funcoes em swift começo

---
+++ Sources/SwiftSandbox/.swift
@@ -0,0 +1,6 @@
+//
+//  aula01.swift
+//  SwiftSandbox
+//
+//  Created by Found on 18/08/26.
+//
--- Sources/SwiftSandbox/main.swift
+++ Sources/SwiftSandbox/main.swift
@@ -1,2 +1,32 @@
-var myName = "Alan"
-print("Hello, \(myName)!")
+
+//FUNCAO 01
+let cards = [1, 2, 3]
+
+/// Obtém a carta no índice especificado da pilha.
+///
+/// - Parameters:
+///   - index: O índice da carta que será obtida.
+///   - stack: A pilha que contém as cartas.
+/// - Returns: A carta localizada no índice especificado.
+func getCard(at index: Int, from stack: [Int]) -> Int {
+    return stack[index]
+}
+
+let card = getCard(at: 0, from: cards)
+print(card)
+
+//FUNCAO 02
+/// Substitui a carta existente no índice especificado por uma nova carta.
+///
+/// - Parameters:
+///   - index: O índice da carta que será substituída.
+///   - stack: A pilha que contém a carta a ser substituída.
+///   - newCard: A nova carta que substituirá a carta existente.
+/// - Returns: Uma nova pilha com a carta no índice especificado substituída por `newCard`.
+func setCard(at index: Int, in stack: [Int], to newCard: Int) -> [Int] {
+    stack.remove(at: index)
+    stack.insert(newCard, at: index)
+}
+
+
+
